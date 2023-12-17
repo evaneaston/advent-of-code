7dd4f7f8ebd94e9b8b6f97159bc105df2042628d
Evan Easton <evan@eeaston.com>
2023-12-17 16:44:22 -0600

    Staging other days

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -9,3 +9,63 @@ pub fn part2(input: DailyInput) -> Result<String, AocError> {
     input.get_input_as_single_string()?;
     Ok("".to_string())
 }
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        day11::{part1, part2},
+        DailyInput, InputType,
+    };
+
+    #[test]
+    fn test_part1_example() {
+        assert_eq!(
+            part1(DailyInput {
+                day: 11,
+                input_type: InputType::Example,
+                number: None,
+            })
+            .unwrap(),
+            "102"
+        );
+    }
+
+    #[test]
+    fn test_part1_challenge() {
+        assert_eq!(
+            part1(DailyInput {
+                day: 11,
+                input_type: InputType::Challenge,
+                number: None,
+            })
+            .unwrap(),
+            ""
+        );
+    }
+
+    #[test]
+    fn test_part2_example() {
+        assert_eq!(
+            part2(DailyInput {
+                day: 11,
+                input_type: InputType::Example,
+                number: None,
+            })
+            .unwrap(),
+            ""
+        );
+    }
+
+    #[test]
+    fn test_part2_challenge() {
+        assert_eq!(
+            part2(DailyInput {
+                day: 11,
+                input_type: InputType::Challenge,
+                number: None,
+            })
+            .unwrap(),
+            ""
+        );
+    }
+}
--- src/day17/mod.rs
+++ src/day17/mod.rs
@@ -1,11 +1,76 @@
-use crate::{AocError,DailyInput};
+use crate::{day15::load, grid::Grid, AocError, DailyInput};
 
 pub fn part1(input: DailyInput) -> Result<String, AocError> {
-    input.get_input_as_single_string()?;
+    let grid = load(input)?;
     Ok("".to_string())
 }
 
 pub fn part2(input: DailyInput) -> Result<String, AocError> {
-    input.get_input_as_single_string()?;
+    let grid = load(input)?;
     Ok("".to_string())
+}
+
+pub(crate) fn load_grid(input: DailyInput) -> Result<Grid, AocError> {
+    let lines = input.get_input_lines()?;
+    Ok(Grid::new(&lines))
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        day17::{part1, part2},
+        DailyInput, InputType,
+    };
+
+    #[test]
+    fn test_part1_example() {
+        assert_eq!(
+            part1(DailyInput {
+                day: 17,
+                input_type: InputType::Example,
+                number: None,
+            })
+            .unwrap(),
+            "102"
+        );
+    }
+
+    #[test]
+    fn test_part1_challenge() {
+        assert_eq!(
+            part1(DailyInput {
+                day: 17,
+                input_type: InputType::Challenge,
+                number: None,
+            })
+            .unwrap(),
+            ""
+        );
+    }
+
+    #[test]
+    fn test_part2_example() {
+        assert_eq!(
+            part2(DailyInput {
+                day: 17,
+                input_type: InputType::Example,
+                number: None,
+            })
+            .unwrap(),
+            ""
+        );
+    }
+
+    #[test]
+    fn test_part2_challenge() {
+        assert_eq!(
+            part2(DailyInput {
+                day: 17,
+                input_type: InputType::Challenge,
+                number: None,
+            })
+            .unwrap(),
+            ""
+        );
+    }
 }
