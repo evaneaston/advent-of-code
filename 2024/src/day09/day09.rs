e3a5dad27656c0ffae14ed3952ecf3a619b55512
Evan Easton <evan@eeaston.com>
2024-11-22 15:14:22 -0600

    More reorg

---
+++ 2024/src/day09/day09.rs
@@ -0,0 +1,69 @@
+use crate::{AocError, DailyInput};
+
+pub fn part1(_input: DailyInput) -> Result<String, AocError> {
+    Ok("".to_string())
+}
+
+pub fn part2(_input: DailyInput) -> Result<String, AocError> {
+    Ok("".to_string())
+}
+
+#[cfg(test)]
+mod test {
+    use super::{part1, part2};
+    use crate::{DailyInput, InputType};
+
+    const DAY: usize = 9;
+
+    #[test]
+    fn test_part1_example() {
+        assert_eq!(
+            part1(DailyInput {
+                day: DAY,
+                input_type: InputType::Example,
+                number: None,
+            })
+                .unwrap(),
+            ""
+        );
+    }
+
+    #[test]
+    fn test_part1_challenge() {
+        assert_eq!(
+            part1(DailyInput {
+                day: DAY,
+                input_type: InputType::Challenge,
+                number: None,
+            })
+                .unwrap(),
+            ""
+        );
+    }
+
+    #[test]
+    fn test_part2_example() {
+        assert_eq!(
+            part2(DailyInput {
+                day: DAY,
+                input_type: InputType::Example,
+                number: None,
+            })
+                .unwrap(),
+            ""
+        );
+    }
+
+    #[test]
+    fn test_part2_challenge() {
+        assert_eq!(
+            part2(DailyInput {
+                day: DAY,
+                input_type: InputType::Challenge,
+                number: None,
+            })
+                .unwrap(),
+            ""
+        );
+    }
+}
