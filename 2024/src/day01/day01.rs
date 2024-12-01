2bbcf2b51d418b05a405d76ef82bffab0fe062ae
Evan Easton <evan@eeaston.com>
2024-12-01 11:48:35 -0600

    2024 Day 1

--- 2024/src/day01/day01.rs
+++ 2024/src/day01/day01.rs
@@ -1,11 +1,49 @@
 use crate::{AocError, DailyInput};
 
-pub fn part1(_input: DailyInput) -> Result<String, AocError> {
-    Ok(_input.get_input_as_single_string()?)
+use std::collections::HashMap;
+
+use lazy_static::lazy_static;
+use regex::Regex;
+
+
+lazy_static! {
+    static ref RE: Regex =
+        Regex::new(r"(\d+)\s+(\d+)").unwrap();
 }
 
-pub fn part2(_input: DailyInput) -> Result<String, AocError> {
-    Ok(_input.get_input_as_single_string()?)
+pub fn part1(input: DailyInput) -> Result<String, AocError> {
+    let (mut left, mut right): (Vec<_>, Vec<_>) = input.get_input_lines()?.iter().map( |l| RE.captures(l).unwrap()).map(|c| {
+        (c.get(1).unwrap().as_str().parse::<i64>().unwrap(), c.get(2).unwrap().as_str().parse::<i64>().unwrap())
+    }).unzip();
+
+    left.sort();
+    right.sort();
+    
+    let sum: i64 = left.iter().zip(right.iter()).map(|(l,r)| {
+         (l-r).abs()
+     }).sum();
+
+    Ok(format!("{sum}"))
+}
+
+pub fn part2(input: DailyInput) -> Result<String, AocError> {
+    let ( left, right): (Vec<_>, Vec<_>) = input.get_input_lines()?.iter().map( |l| RE.captures(l).unwrap()).map(|c| {
+        (c.get(1).unwrap().as_str().parse::<i64>().unwrap(), c.get(2).unwrap().as_str().parse::<i64>().unwrap())
+    }).unzip();
+
+    let mut right_counts = HashMap::new();
+    for &num in &right {
+        *right_counts.entry(num).or_insert(0) += 1;
+    }
+
+    let sum: i64 = left.iter().map(|l| {
+        match right_counts.get(l) {
+            Some(c) => l*c,
+            None => 0,
+        }
+    }).sum();
+
+    Ok(format!("{sum}"))
 }
 
 #[cfg(test)]
@@ -24,7 +62,7 @@ mod test {
                 number: None,
             })
             .unwrap(),
-            "A"
+            "11"
         );
     }
 
@@ -37,7 +75,7 @@ mod test {
                 number: None,
             })
             .unwrap(),
-            "B"
+            "2000468"
         );
     }
 
@@ -50,7 +88,7 @@ mod test {
                 number: None,
             })
             .unwrap(),
-            "A"
+            "31"
         );
     }
 
@@ -63,7 +101,7 @@ mod test {
                 number: None,
             })
             .unwrap(),
-            "B"
+            "18567089"
         );
     }
 }
