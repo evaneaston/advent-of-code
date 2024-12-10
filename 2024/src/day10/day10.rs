94f283b10ef509cc0c1ea1619ad07f06fb88441b
Evan Easton <evan@eeaston.com>
2024-12-10 00:38:15 -0600

    Day 10

--- 2024/src/day10/day10.rs
+++ 2024/src/day10/day10.rs
@@ -1,11 +1,66 @@
-use crate::{AocError, DailyInput};
+use std::collections::HashSet;
 
-pub fn part1(_input: DailyInput) -> Result<String, AocError> {
-    Ok("".to_string())
+use crate::{
+    coord::{Direction, RowCol},
+    grid::Grid,
+    AocError, DailyInput,
+};
+
+fn score(g: &Grid, start: RowCol) -> usize {
+    let mut nines = HashSet::<RowCol>::new();
+    climb(g, b'0', &start, &mut nines);
+    nines.len()
+}
+fn climb(g: &Grid, last_val: u8, coord: &RowCol, nines: &mut HashSet<RowCol>) {
+    for dir in [Direction::N, Direction::S, Direction::E, Direction::W] {
+        let next = coord.plus(&dir);
+        if let Some(v) = g.get(next) {
+            if v == last_val + 1 {
+                if v == b'9' {
+                    nines.insert(next);
+                } else {
+                    climb(g, v, &next, nines);
+                }
+            }
+        }
+    }
+}
+pub fn part1(input: DailyInput) -> Result<String, AocError> {
+    let g = Grid::new(&input.get_input_lines()?);
+    let index = g.find(HashSet::from_iter([b'0']));
+    let trailheads = index.get(&b'0').unwrap();
+
+    let answer = trailheads.iter().map(|&start| score(&g, start)).sum::<usize>();
+    Ok(format!("{answer}"))
 }
 
-pub fn part2(_input: DailyInput) -> Result<String, AocError> {
-    Ok("".to_string())
+fn rating(g: &Grid, start: RowCol) -> usize {
+    let mut count = 0_usize;
+    climb_for_rating(g, b'0', &start, &mut count);
+    count
+}
+fn climb_for_rating(g: &Grid, last_val: u8, coord: &RowCol, count: &mut usize) {
+    for dir in [Direction::N, Direction::S, Direction::E, Direction::W] {
+        let next = coord.plus(&dir);
+        if let Some(v) = g.get(next) {
+            if v == last_val + 1 {
+                if v == b'9' {
+                    *count += 1;
+                } else {
+                    climb_for_rating(g, v, &next, count);
+                }
+            }
+        }
+    }
+}
+
+pub fn part2(input: DailyInput) -> Result<String, AocError> {
+    let g = Grid::new(&input.get_input_lines()?);
+    let index = g.find(HashSet::from_iter([b'0']));
+    let trailheads = index.get(&b'0').unwrap();
+
+    let answer = trailheads.iter().map(|&start| rating(&g, start)).sum::<usize>();
+    Ok(format!("{answer}"))
 }
 
 #[cfg(test)]
@@ -23,8 +78,8 @@ mod test {
                 input_type: InputType::Example,
                 number: None,
             })
-                .unwrap(),
-            ""
+            .unwrap(),
+            "36"
         );
     }
 
@@ -36,8 +91,8 @@ mod test {
                 input_type: InputType::Challenge,
                 number: None,
             })
-                .unwrap(),
-            ""
+            .unwrap(),
+            "512"
         );
     }
 
@@ -49,8 +104,8 @@ mod test {
                 input_type: InputType::Example,
                 number: None,
             })
-                .unwrap(),
-            ""
+            .unwrap(),
+            "81"
         );
     }
 
@@ -62,8 +117,8 @@ mod test {
                 input_type: InputType::Challenge,
                 number: None,
             })
-                .unwrap(),
-            ""
+            .unwrap(),
+            "1045"
         );
     }
 }
