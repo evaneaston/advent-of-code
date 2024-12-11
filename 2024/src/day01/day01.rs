9e4008f6d2fee24887210b47e22f83d0a341c4d4
Evan Easton <evan.easton@datasite.com>
2024-12-11 15:41:31 -0600

    Use recap

--- 2024/src/day01/day01.rs
+++ 2024/src/day01/day01.rs
@@ -2,32 +2,41 @@ use crate::{AocError, DailyInput};
 
 use std::collections::HashMap;
 
+use recap::Recap;
 use lazy_static::lazy_static;
 use regex::Regex;
-
+use serde::Deserialize;
 
 lazy_static! {
     static ref RE: Regex =
         Regex::new(r"(\d+)\s+(\d+)").unwrap();
+    }
+
+#[derive(Debug, Deserialize, PartialEq, Recap)]
+#[recap(regex = r#"(?P<left>\d+)\s+(?P<right>\d+)"#)]
+struct Row {
+    left: i64,
+    right: i64,
 }
 
 pub fn part1(input: DailyInput) -> Result<String, AocError> {
-    let (mut left, mut right): (Vec<_>, Vec<_>) = input.get_input_lines()?.iter().map( |l| RE.captures(l).unwrap()).map(|c| {
-        (c.get(1).unwrap().as_str().parse::<i64>().unwrap(), c.get(2).unwrap().as_str().parse::<i64>().unwrap())
-    }).unzip();
+    let (mut left, mut right): (Vec<_>, Vec<_>) = input.get_input_lines()?.iter()
+        .map(|l| l.parse::<Row>().unwrap())
+        .map(|r| (r.left, r.right))
+        .unzip();
 
     left.sort();
     right.sort();
-    
-    let sum: i64 = left.iter().zip(right.iter()).map(|(l,r)| {
-         (l-r).abs()
-     }).sum();
 
-    Ok(format!("{sum}"))
+    let sum: i64 = left.iter().zip(right.iter()).map(|(l, r)| {
+        (l - r).abs()
+    }).sum();
+
+    Ok(sum.to_string())
 }
 
 pub fn part2(input: DailyInput) -> Result<String, AocError> {
-    let ( left, right): (Vec<_>, Vec<_>) = input.get_input_lines()?.iter().map( |l| RE.captures(l).unwrap()).map(|c| {
+    let (left, right): (Vec<_>, Vec<_>) = input.get_input_lines()?.iter().map(|l| RE.captures(l).unwrap()).map(|c| {
         (c.get(1).unwrap().as_str().parse::<i64>().unwrap(), c.get(2).unwrap().as_str().parse::<i64>().unwrap())
     }).unzip();
 
@@ -38,12 +47,12 @@ pub fn part2(input: DailyInput) -> Result<String, AocError> {
 
     let sum: i64 = left.iter().map(|l| {
         match right_counts.get(l) {
-            Some(c) => l*c,
+            Some(c) => l * c,
             None => 0,
         }
     }).sum();
 
-    Ok(format!("{sum}"))
+    Ok(sum.to_string())
 }
 
 #[cfg(test)]
@@ -61,7 +70,7 @@ mod test {
                 input_type: InputType::Example,
                 number: None,
             })
-            .unwrap(),
+                .unwrap(),
             "11"
         );
     }
@@ -74,7 +83,7 @@ mod test {
                 input_type: InputType::Challenge,
                 number: None,
             })
-            .unwrap(),
+                .unwrap(),
             "2000468"
         );
     }
@@ -87,7 +96,7 @@ mod test {
                 input_type: InputType::Example,
                 number: None,
             })
-            .unwrap(),
+                .unwrap(),
             "31"
         );
     }
@@ -100,7 +109,7 @@ mod test {
                 input_type: InputType::Challenge,
                 number: None,
             })
-            .unwrap(),
+                .unwrap(),
             "18567089"
         );
     }
